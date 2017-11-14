5bd66d2cf918b797f8ee4ccb91159cebb1a59f7d
Valentin TOUFFET <vtouffet@e2r12p12.42.fr>
2017-11-14 14:27:03 +0100

    Add photo

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,32 +6,23 @@
 /*   By: vtouffet <vtouffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 14:18:05 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/09 10:52:32 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/12 15:11:30 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
-	size_t	size;
+	char	*str;
+	int		len;
 
-	if (s1 && s2)
-		size = (size_t)(ft_strlen((char*)s1) + ft_strlen((char*)s2));
-	else if (s1)
-		size = (size_t)(ft_strlen((char*)s1));
-	else if (s2)
-		size = (size_t)(ft_strlen((char*)s2));
-	else
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(*str) * (len + 1));
+	if (str == NULL)
 		return (NULL);
-	if (!(result = ft_memalloc(size)))
-		return (NULL);
-	if (s1)
-		result = ft_strcpy(result, (char*)s1);
-	else
-		result = ft_strcpy(result, (char*)s2);
-	if (s1 && s2)
-		result = ft_strcat(result, (char*)s2);
-	return (result);
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+	return (str);
 }
