b78a30c071d3e38ba13668f732c1d8fb0ccf0f60
Valentin TOUFFET <vtouffet@e2r11p11.42.fr>
2017-11-16 19:59:06 +0100

    Rewrite

--- fdf/includes/fdf.h
+++ fdf/includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: vtouffet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/13 12:18:12 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/16 17:57:59 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/16 19:35:48 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # define ZOOM 5
 # define INCLINATION 1.7
 # define HEIGHT_AMPLIFIER 2
-# define ARROW_AMPLIFIER 5
+# define ARROW_AMPLIFIER 3
 # define DEFAULT_COLOR 0x00e8f7ff
 # define MID_COLOR 0x0082c1e0
 # define MAX_COLOR 0x0003547c
@@ -66,9 +66,11 @@ typedef struct	s_env
 
 int 	ft_open_file(const char *filename);
 t_list	*ft_read(int fd, t_options options);
-t_point	*ft_new_point(int x, int y, int h, t_options options, int index);
+t_point	*ft_new_point(int x, int y, int h, int index);
 void	ft_throw_error(void);
 int		ft_listen_key(int keycode, void *param);
+t_list	*ft_move_points(t_list *points, t_options options, t_point move,
+						  int old_zoom);
 void	ft_display(t_list *points, t_options options, t_env env);
 
 t_list	*ft_debug_points(t_list *point);
--- fdf/srcs/listener.c
+++ fdf/srcs/listener.c
@@ -6,76 +6,38 @@
 /*   By: vtouffet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 14:58:36 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/16 18:26:40 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/16 19:40:22 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-t_list	*ft_zoom(t_env *env, int old_zoom)
-{
-	t_list	*ptr;
-	t_list	*points;
-	t_point	*point;
-
-	points = env->points;
-	ptr = points;
-	while (ptr)
-	{
-		point = ptr->content;
-		point->x = point->x / old_zoom;
-		point->y = (point->y + point->h) / old_zoom;
-		point->h /= env->options.amplifier;
-		ptr->content = ft_new_point(point->x, point->y, point->h, env->options, 0);
-		free(point);
-		ptr = ptr->next;
-	}
-	return (points);
-}
-
-t_list	*ft_rewrite_points(t_env *env, int x, int y)
-{
-	t_list	*ptr;
-	t_list	*points;
-	t_point	*point;
-
-	points = env->points;
-	ptr = points;
-	while (ptr)
-	{
-		point = ptr->content;
-		point->x = point->x / env->options.zoom;
-		point->y = (point->y + point->h) / env->options.zoom;
-		point->h /= env->options.amplifier;
-		ptr->content = ft_new_point(point->x + x, point->y + y, point->h,
-									env->options, 0);
-		free(point);
-		ptr = ptr->next;
-	}
-	return (points);
-}
-
 void	ft_handle_arrows(int keycode, t_env *env)
 {
+	t_point move;
+
+	move.x = 0;
+	move.y = 0;
 	if (keycode == KEYCODE_RIGHT_ARROW)
-		ft_display(ft_rewrite_points(env, ARROW_AMPLIFIER, 0),
-					env->options, *env);
+		move.x = ARROW_AMPLIFIER;
 	else if (keycode == KEYCODE_LEFT_ARROW)
-		ft_display(ft_rewrite_points(env, -ARROW_AMPLIFIER, 0),
-					env->options, *env);
+		move.x = -ARROW_AMPLIFIER;
 	else if (keycode == KEYCODE_UP_ARROW)
-		ft_display(ft_rewrite_points(env, 0, -ARROW_AMPLIFIER),
-					env->options, *env);
+		move.y = -ARROW_AMPLIFIER;
 	else if (keycode == KEYCODE_DOWN_ARROW)
-		ft_display(ft_rewrite_points(env, 0, ARROW_AMPLIFIER),
-					env->options, *env);
+		move.y = ARROW_AMPLIFIER;
+	ft_move_points(env->points, env->options, move, env->options.zoom);
+	ft_display(env->points, env->options, *env);
 }
 
 int		ft_listen_key(int keycode, void *param)
 {
 	t_env	*env;
 	int		tmp;
+	t_point	move;
 
+	move.x = 0;
+	move.y = 0;
 	env = (t_env*)param;
 	if (keycode == KEYCODE_ESC)
 		exit(0);
@@ -85,7 +47,8 @@ int		ft_listen_key(int keycode, void *param)
 		if (tmp + ZOOM > 100)
 			return (0);
 		env->options.zoom += ZOOM;
-		ft_display(ft_zoom(env, tmp), env->options, *env);
+		ft_move_points(env->points, env->options, move, tmp);
+		ft_display(env->points, env->options, *env);
 	}
 	else if (keycode == KEYCODE_ZOOM_OUT)
 	{
@@ -93,7 +56,8 @@ int		ft_listen_key(int keycode, void *param)
 		if (tmp - ZOOM <= 0)
 			return (0);
 		env->options.zoom -= ZOOM;
-		ft_display(ft_zoom(env, tmp), env->options, *env);
+		ft_move_points(env->points, env->options, move, tmp);
+		ft_display(env->points, env->options, *env);
 	}
 	else
 		ft_handle_arrows(keycode, env);
