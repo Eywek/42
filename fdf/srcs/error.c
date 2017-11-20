02dcd803e56d5765f8341f94622751fbc9de904c
Valentin TOUFFET <vtouffet@e3r10p13.42.fr>
2017-11-20 10:32:20 +0100

    Add some errors

--- fdf/srcs/error.c
+++ fdf/srcs/error.c
@@ -6,12 +6,24 @@
 /*   By: vtouffet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 16:34:45 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/16 16:39:17 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/20 10:29:20 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
+#include "../includes/fdf.h"
+
+int		ft_is_valid(char *str)
+{
+	while (*str)
+	{
+		if (*str != '-' && *str != '+' && !ft_isdigit(*str))
+			return (0);
+		++str;
+	}
+	return (1);
+}
 
 void	ft_throw_error(void)
 {
