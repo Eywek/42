5eea131cb42a4dbbd3b2d76d40dc9f821810a45e
Valentin TOUFFET <vtouffet@e2r11p10.42.fr>
2017-11-22 18:03:39 +0100

    some fixs

--- ft_printf/srcs/types/numbers_l.c
+++ ft_printf/srcs/types/numbers_l.c
@@ -6,7 +6,7 @@
 /*   By: vtouffet <vtouffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 11:18:26 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/22 11:18:37 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/22 17:54:02 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,12 @@ int flag_D(va_list args, t_flags flags)
 
 int flag_O(va_list args, t_flags flags)
 {
+	int size;
+
 	(void)flags;
-	(void)args;
-	return (0);
+	ft_putnbr_base_unsigned_long(va_arg(args, unsigned long int),
+								 "01234567", 8, &size);
+	return (size);
 }
 
 int flag_U(va_list args, t_flags flags)
