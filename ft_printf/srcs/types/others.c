d7ce7b88862f0d225cc83a36d6dbe2ced1f9f67c
Valentin TOUFFET <vtouffet@e2r11p10.42.fr>
2017-11-22 17:51:58 +0100

    lol

--- ft_printf/srcs/types/others.c
+++ ft_printf/srcs/types/others.c
@@ -6,7 +6,7 @@
 /*   By: vtouffet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 13:14:28 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/21 15:39:10 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/22 17:16:28 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,18 @@
 
 int	flag_percentage(va_list args, t_flags flags)
 {
-	(void)flags;
+	int		width;
+
 	(void)args;
-	write(STDOUT, "%", 1);
-	return (1);
+	if (flags.width)
+	{
+		if (flags.minus)
+			write(STDOUT, "%", 1);
+		width = 0;
+		while (width++ < flags.width - 1)
+			write(STDOUT, (flags.zero) ? "0" : " ", 1);
+	}
+	if (!flags.width || !flags.minus)
+		write(STDOUT, "%", 1);
+	return (1 + flags.width);
 }
--- ft_printf/srcs/utils/bases.c
+++ ft_printf/srcs/utils/bases.c
@@ -6,10 +6,11 @@
 /*   By: vtouffet <vtouffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/22 13:40:09 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/22 13:53:43 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/22 16:17:20 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdint.h>
 #include "../../includes/core.h"
 
 void	ft_putnbr_base_unsigned(unsigned int nbr, char *str,
@@ -27,5 +28,56 @@ void	ft_putnbr_base_unsigned_long(unsigned long int nbr, char *str,
 	*size += 1;
 	if (nbr >= str_length)
 		ft_putnbr_base_unsigned_long(nbr / str_length, str, str_length, size);
+	ft_putchar_fd(str[nbr % str_length], STDOUT);
+}
+
+void	ft_putnbr_base_unsigned_long_long(unsigned long long int nbr, char *str,
+								 unsigned long long int str_length, int *size)
+{
+	*size += 1;
+	if (nbr >= str_length)
+		ft_putnbr_base_unsigned_long_long(nbr / str_length, str, str_length,
+										  size);
+	ft_putchar_fd(str[nbr % str_length], STDOUT);
+}
+
+void	ft_putnbr_base_short_int(short int nbr, char *str,
+								 short int str_length, int *size)
+{
+	*size += 1;
+	if (nbr >= str_length)
+		ft_putnbr_base_short_int(nbr / str_length, str, str_length,
+										  size);
+	ft_putchar_fd(str[nbr % str_length], STDOUT);
+}
+
+void	ft_putnbr_base_uintmax_t(uintmax_t nbr, char *str,
+								uintmax_t str_length, int *size)
+{
+	*size += 1;
+	if (nbr >= str_length)
+		ft_putnbr_base_uintmax_t(nbr / str_length, str, str_length,
+										  size);
+	ft_putchar_fd(str[nbr % str_length], STDOUT);
+}
+
+
+void	ft_putnbr_base_unsigned_char(unsigned char nbr, char *str,
+										  unsigned char str_length, int *size)
+{
+	*size += 1;
+	if (nbr >= str_length)
+		ft_putnbr_base_unsigned_char(nbr / str_length, str, str_length,
+										  size);
+	ft_putchar_fd(str[nbr % str_length], STDOUT);
+}
+
+void	ft_putnbr_base_size_t(size_t nbr, char *str,
+									 size_t str_length, int *size)
+{
+	*size += 1;
+	if (nbr >= str_length)
+		ft_putnbr_base_size_t(nbr / str_length, str, str_length,
+									 size);
 	ft_putchar_fd(str[nbr % str_length], STDOUT);
 }
