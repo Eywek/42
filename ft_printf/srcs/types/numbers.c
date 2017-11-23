3348346e42b195a8390517d3e37466595d41fa32
Eywek <contact@eywek.fr>
2017-11-23 20:36:18 +0100

    YYYYYEEEEAAHHHHH HANDLE UNSIGNED AND MULTIPLE TYPES

--- ft_printf/srcs/types/numbers.c
+++ ft_printf/srcs/types/numbers.c
@@ -6,7 +6,7 @@
 /*   By: vtouffet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 13:14:28 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/23 20:18:49 by valentin         ###   ########.fr       */
+/*   Updated: 2017/11/23 20:29:38 by valentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,34 +36,7 @@ int	flag_d(va_list args, t_flags flags)
 	return (size + (flags.width - size > 0 ? flags.width - size : 0));
 }
 
-// TODO: Precision / Width / Flags
-
-int	ft_pad_nb(va_list args, t_flags flags, char *base, char *hash_key_content)
-{
-	int size;
-	int width;
-
-	size = 0;
-	intmax_t nb = ft_get_nb(args, flags);
-	ft_get_number_size(nb, ft_strlen(base), &size);
-	if (nb == 0)
-		flags.hash_key = 0;
-	if (flags.hash_key && (size += 2) && (!flags.width || flags.minus || flags.zero))
-		write(STDOUT, hash_key_content, 2);
-	if (flags.width > 0 && (width = 0) == 0)
-	{
-		if (flags.minus)
-			ft_putnbr_base_intmax_t(nb, base, ft_strlen(base));
-		while (width++ < flags.width - size)
-			write(STDOUT, (flags.zero && !flags.minus) ? "0" : " ", 1);
-	}
-	if (flags.hash_key && flags.width && !flags.minus && !flags.zero)
-		write(STDOUT, hash_key_content, 2);
-	if (!flags.width || !flags.minus)
-		ft_putnbr_base_intmax_t(nb, base, ft_strlen(base));
-	return (flags.width - size > 0 ? flags.width : size);
-}
-
+// TODO: Precision / Types
 
 int flag_o(va_list args, t_flags flags)
 {
