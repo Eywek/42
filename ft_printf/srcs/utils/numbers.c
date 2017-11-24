bd997aeb8fa158591858ce3a1a937a8c9c34e057
Eywek <contact@eywek.fr>
2017-11-24 17:06:02 +0100

    handle precision on x, X, u, o

--- ft_printf/srcs/utils/numbers.c
+++ ft_printf/srcs/utils/numbers.c
@@ -6,7 +6,7 @@
 /*   By: valentin <null>                            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 18:20:18 by valentin          #+#    #+#             */
-/*   Updated: 2017/11/23 20:35:11 by valentin         ###   ########.fr       */
+/*   Updated: 2017/11/24 17:04:07 by valentin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,40 @@ uintmax_t	ft_get_nb_u(va_list args, t_flags flags)
 	return (nbr);
 }
 
+/*
+ ** Display spaces or zeros if needed
+ **
+*/
+
+void		ft_display_padding(t_flags *flags, uintmax_t nb, int size,
+							   char *base)
+{
+	int			width;
+
+	if (flags->precision <= 0 && flags->width <= 0)
+		return ;
+	width = 0;
+	if (flags->precision > flags->width)
+	{
+		flags->width = flags->precision;
+		flags->zero = 1;
+		flags->minus = 0;
+	}
+	if (flags->minus && flags->precision != -1)
+		ft_putnbr_base_intmax_t_u(nb, base, ft_strlen(base));
+	while (width++ < flags->width - size)
+		write(STDOUT, (flags->zero && !flags->minus) ? "0" : " ", 1);
+}
+
+/*
+ ** Display hash_key_content, add some width, handle precision
+ ** Return the correct size
+*/
+
 int			ft_pad_nb(va_list args, t_flags flags, char *base,
 						 char *hash_key_content)
 {
 	int			size;
-	int			width;
 	uintmax_t	nb;
 
 	size = 0;
@@ -72,16 +101,12 @@ int			ft_pad_nb(va_list args, t_flags flags, char *base,
 	if (flags.hash_key && (size += 2) && (!flags.width || flags.minus ||
 			flags.zero))
 		write(STDOUT, hash_key_content, 2);
-	if (flags.width > 0 && (width = 0) == 0)
-	{
-		if (flags.minus)
-			ft_putnbr_base_intmax_t_u(nb, base, ft_strlen(base));
-		while (width++ < flags.width - size)
-			write(STDOUT, (flags.zero && !flags.minus) ? "0" : " ", 1);
-	}
+	if (flags.precision == -1)
+		size = 0;
+	ft_display_padding(&flags, nb, size, base);
 	if (flags.hash_key && flags.width && !flags.minus && !flags.zero)
 		write(STDOUT, hash_key_content, 2);
-	if (!flags.width || !flags.minus)
+	if ((!flags.width || !flags.minus) && flags.precision != -1)
 		ft_putnbr_base_intmax_t_u(nb, base, ft_strlen(base));
 	return (flags.width - size > 0 ? flags.width : size);
 }
