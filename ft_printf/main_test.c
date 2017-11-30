dccf609dd92f1b8771891e230d7b401d92f80bb2
Valentin TOUFFET <vtouffet@e2r11p13.42.fr>
2017-11-30 16:57:28 +0100

    fix colors

--- ft_printf/main_test.c
+++ ft_printf/main_test.c
@@ -6,7 +6,7 @@
 /*   By: vtouffet <vtouffet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 20:25:54 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/11/30 15:01:18 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/11/30 16:45:37 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,9 @@ void	speed_cmp(void)
 int		main(int argc, char *argv[])
 {
 	//printf("Le fichier {cyan}%s{eoc} contient : {red}%s{eoc}\n", "yolo.txt", "'contenu'");
-	ft_printf("Le fichier {cyan}%s{eoc} contient : {red}%s{eoc}", "yolo.txt", "'contenu'");
+	//ft_printf("Le fichier {cyan}%s{eoc} contient : {red}%s{eoc}", "yolo.txt", "'contenu'");
+	ft_printf("{blue}[{cyan}IMPORTANT{blue}] {eoc}Voici un message {red}important{eoc}, en voici la raison '{yellow}test{eoc}'");
+	//ft_printf("{green}[{red}IMPORTANT");
 //	speed();
 //	speed_cmp();
 	//ft_printf("%llu\n", ULLONG_MAX);
