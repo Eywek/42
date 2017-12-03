2a6810b52a745c574455f49d00ca1af55d1a908b
Valentin TOUFFET <vtouffet@e2r11p8.42.fr>
2017-12-03 16:56:38 +0100

    add libft

---
+++ ft_ls/libs/libft/ft_lstaddend.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstaddend.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtouffet <vtouffet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/21 17:04:38 by vtouffet          #+#    #+#             */
+/*   Updated: 2017/11/09 10:48:24 by vtouffet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstaddend(t_list **alst, t_list *new)
+{
+	t_list	*ptr;
+
+	ptr = *alst;
+	while (ptr->next)
+		ptr = ptr->next;
+	ptr->next = new;
+}
