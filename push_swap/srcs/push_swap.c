e2e756d1cd6a38650b893062259eae3f67ce4109
Valentin TOUFFET <vtouffet@e2r10p13.42.fr>
2017-12-07 19:27:13 +0100

    makefile

---
+++ push_swap/srcs/push_swap.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtouffet <vtouffet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/07 19:09:55 by vtouffet          #+#    #+#             */
+/*   Updated: 2017/12/07 19:26:30 by vtouffet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	ft_putstr("Wait please\n");
+	return (EXIT_SUCCESS);
+}
