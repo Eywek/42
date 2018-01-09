af4048b714d5d14ca87286d92d2a75cd4d1e7384
Valentin TOUFFET <vtouffet@e3r10p11.42.fr>
2018-01-09 18:51:16 +0100

    fix

--- piscine-php/day01/ex09/ssap2.php
+++ piscine-php/day01/ex09/ssap2.php
@@ -8,26 +8,28 @@ foreach ($array as $el)
         $words[] = $e;
 
 // Sorting alpha
-
 usort($words, function ($a, $b) {
-    return strcmp(strtolower($a), strtolower($b));
+    $a = strtolower($a);
+    $b = strtolower($b);
+    $customOrder = str_split("abcdefghijklmnopqrstuvwxyz0123456789");
+    $customOrder = array_flip($customOrder);
+    $b = str_split($b);
+    foreach (str_split($a) as $i => $char) {
+        if ($char === $b[$i])
+            continue;
+        if (!isset($customOrder[$char]) && !isset($customOrder[$b[$i]]))
+            return strcmp($char, $b[$i]);
+        if (!isset($customOrder[$char]))
+            return 1;
+        else if (!isset($customOrder[$b[$i]]))
+            return -1;
+        if ($customOrder[$char] === $customOrder[$b[$i]])
+            return 0;
+        return ($customOrder[$char] > $customOrder[$b[$i]]) ? 1 : -1;
+    }
+    return 0;
 });
-$words = array_values($words); // reset keys
 
-// Display alpha
-foreach ($words as $i => $word) {
-    if (ctype_alpha($word))
+// Display
+foreach ($words as $word)
         echo "$word\n";
-}
-
-// Display numeric
-foreach ($words as $i => $word) {
-    if (is_numeric($word))
-        echo "$word\n";
-}
-
-// Others
-foreach ($words as $i => $word) {
-    if (!ctype_alpha($word) && !is_numeric($word))
-        echo "$word\n";
-}
