a8cf373c45d1204ed256c6164c2df3b6f587fa8b
Valentin TOUFFET <vtouffet@e3r10p11.42.fr>
2018-01-09 14:59:51 +0100

    finish ex02

--- piscine-php/day02/ex02/loupe.php
+++ piscine-php/day02/ex02/loupe.php
@@ -4,28 +4,30 @@ if ($argc < 2 || !file_exists($argv[1]))
     exit(1);
 $content = file_get_contents($argv[1]);
 echo preg_replace_callback('/<a [^>]+.*<\/a>/i', function ($matches) {
-    var_dump($matches);
     // Setup a
     $result = $matches[0];
 
-    // Replace title
-    $result = preg_replace_callback('/title="(.*)"/i', function ($matches) {
-        var_dump($matches);
-        return 'title="' . strtoupper($matches[1]) . '"';
+    // Replace content
+    $result = preg_replace_callback('/>.*</i', function ($matches) {
+        // To upper
+        $result = strtoupper($matches[0]);
+        if (strpos(substr($matches[0], 1, strlen($matches[0]) - 2), '<') !== false) {
+            // With html tags we need to lower html tags
+            $result = preg_replace_callback('/<([^>]+)(>([^<>]*)<\/|)([^>]*)>/i', function ($matches) {
+                $return = "<" . strtolower($matches[1]) . ">";
+                if (!empty($matches[4]))
+                    $return .= "$matches[3]</" . strtolower($matches[4]) . ">";
+                return $return;
+            }, $result);
+        }
+
+        // Return
+        return $result;
     }, $result);
 
-    // Replace content
-//    $result = preg_replace_callback('/>.*</i', function ($matches) {
-//        var_dump($matches);
-//        return strtoupper($matches[0]);
-//    }, $result);
-
-    $result = preg_replace_callback('/>.*</i', function ($matches) {
-        $callback = function ($match) {
-            return strtoupper($match[1]);
-        };
-        $regex = '/(\w+(?=&)|(?<=;)\w+)/';
-        return preg_replace_callback($regex, $callback, $matches[0]);
+    // Replace title
+    $result = preg_replace_callback('/title="(.*)"/i', function ($matches) {
+        return 'title="' . strtoupper($matches[1]) . '"';
     }, $result);
 
     return ($result);
