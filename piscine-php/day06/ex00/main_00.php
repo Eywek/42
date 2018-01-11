9f421ff276f2b3a72f1bb2cde1e19828e94e7d9a
Valentin TOUFFET <vtouffet@e2r10p10.42.fr>
2018-01-11 13:37:21 +0100

    day06/ex00

---
+++ piscine-php/day06/ex00/main_00.php
@@ -0,0 +1,43 @@
+#!/usr/bin/php
+<?php
+
+require_once 'Color.class.php';
+
+
+print( Color::doc() );
+Color::$verbose = True;
+
+$red     = new Color( array( 'red' => 0xff, 'green' => 0   , 'blue' => 0    ) );
+$green   = new Color( array( 'rgb' => 255 << 8 ) );
+$blue    = new Color( array( 'red' => 0   , 'green' => 0   , 'blue' => 0xff ) );
+
+$yellow  = $red->add( $green );
+$cyan    = $green->add( $blue );
+$magenta = $blue->add( $red );
+
+$white   = $red->add( $green )->add( $blue );
+
+print( $red     . PHP_EOL );
+print( $green   . PHP_EOL );
+print( $blue    . PHP_EOL );
+print( $yellow  . PHP_EOL );
+print( $cyan    . PHP_EOL );
+print( $magenta . PHP_EOL );
+print( $white   . PHP_EOL );
+
+Color::$verbose = False;
+
+$black = $white->sub( $red )->sub( $green )->sub( $blue );
+print( 'Black: ' . $black . PHP_EOL );
+
+Color::$verbose = True;
+
+$darkgrey = new Color( array( 'rgb' => (10 << 16) + (10 << 8) + 10 ) );
+print( 'darkgrey: ' . $darkgrey . PHP_EOL );
+$lightgrey = $darkgrey->mult( 22.5 );
+print( 'lightgrey: ' . $lightgrey . PHP_EOL );
+
+$random = new Color( array( 'red' => 12.3, 'green' => 31.2, 'blue' => 23.1 ) );
+print( 'random: ' . $random . PHP_EOL );
+
+?>
