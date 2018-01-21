39dad927bf153717c2e89727b879c0112321ef8a
Eywek <contact@eywek.fr>
2018-01-21 18:53:14 +0100

    Camagru: Fix \url() method (substr current path)

--- camagru/app/Functions/utils.php
+++ camagru/app/Functions/utils.php
@@ -28,8 +28,8 @@ function url($path = '/')
     $currentUrl = "http" . (isset($_SERVER['HTTPS']) ? 's' : '') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
     if (!isset($_GET['path']))
         $_GET['path'] = '';
-    $_GET['path'] = '/' . $_GET['path'];
-    $currentUrl = substr($currentUrl, 0, -strlen($_GET['path']));
+    $_GET['path'] = '/' . trim($_GET['path'], '/');
+    $currentUrl = substr($currentUrl, 0, -strlen($_GET['path'])) . '/';
     if ($path[0] === '/')
         $path = substr($path, 1);
     if (REWRITE_URL)
